bab983d5f3af989fd9a81fa364bc9c0a66d3fc34
nateos <sodjanathan@gmail.com>
2022-10-16 21:44:27 +0100

    feat: create note endpoint

--- controllers/notes.js
+++ controllers/notes.js
@@ -5,8 +5,15 @@ const getAllNotes = async (req, res) => {
   const allNotes = await Note.find({});
   res.status(200).json({ allNotes, total: allNotes.length });
 };
-
-const createNote = () => {};
+// Creating note
+const createNote = async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(200).json({ note });
+  } catch (error) {
+    throw error;
+  }
+};
 const getNote = () => {};
 const deleteNote = () => {};
 const updateNote = () => {};
