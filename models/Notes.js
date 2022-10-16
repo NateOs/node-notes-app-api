d8b0d47ad814796a2ed5ec333403ea0a2c1ef01a
nateos <sodjanathan@gmail.com>
2022-10-16 21:43:20 +0100

    chore: create schema, npm stuff

--- models/Notes.js
+++ models/Notes.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const NoteSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: [true, 'must provide a title'],
-		trim: true,
-		maxlength: [20, 'character length must not exceed 20 characters']
-	},
-	content: {
-		type: String,
-		required: false,
-		maxlength: [200, 'character length must not exceed 200 characters'],
-	}
-})
+  title: {
+    type: String,
+    required: [true, "must provide a title"],
+    trim: true,
+    maxlength: [20, "character length must not exceed 20 characters"],
+  },
+  content: {
+    type: String,
+    required: false,
+    maxlength: [200, "character length must not exceed 200 characters"],
+  },
+});
 
-module.exports = mongoose.model('Note', NoteSchema);
+module.exports = mongoose.model("Note", NoteSchema);
