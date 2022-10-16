2cb7c6cc5c138db19aa74112af74f5a75f59aa9c
nateos <sodjanathan@gmail.com>
2022-10-16 21:42:29 +0100

    fix: wrong order of middleware= req.body bug

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db/connect");
-const notes = require("./routes/notes");
+const notesRouter = require("./routes/notes");
 
 const app = express();
+app.use(express.json());
 
-const port = 3000;
+app.use("/api/v1/notes", notesRouter);
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to your own node app");
 });
-
-app.use("/api/v1/notes", notes);
 
 const start = async () => {
   try {
