ea3d31dbec8d18a4561da4f8586da6364c31ad2e
nateos <sodjanathan@gmail.com>
2022-10-19 15:07:55 +0100

    chore: added express async errors package

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const connectDB = require("./db/connect");
 const notesRouter = require("./routes/notes");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
 const app = express();
 app.use(express.json());
@@ -19,8 +22,12 @@ const start = async () => {
     await connectDB(process.env.MONGODB_URI);
     app.listen(port, console.log("Note app listening on port " + port));
   } catch (err) {
-    console.error(err);
+    console.error("Failed to connect:", err);
   }
 };
+
+// extra middleware, placed here so that it runs
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 start();
